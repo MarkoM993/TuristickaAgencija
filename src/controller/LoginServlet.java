66d180eae7ff0fce7935bd3347c1fb1cfb8c04b2
okkursevi9 <okkursevi9@UC5PC3>
2020-01-21 20:12:32 +0100

    login pocetak

--- src/controller/LoginServlet.java
+++ src/controller/LoginServlet.java
@@ -7,35 +7,32 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.User;
+import service.LoginService;
+
 /**
  * Servlet implementation class LoginServlet
  */
 @WebServlet(description = "ovo je servlet za login", urlPatterns = { "/LoginServlet" })
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		String userName = request.getParameter("userName");
+		String password = request.getParameter("password");
+		
+		LoginService service = new LoginService();
+		
+		User loginUser = service.vratiUseraLogin(userName,password);
+		
+		if(loginUser != null) {
+			
+		}else {
+			response.sendRedirect("htmlFajlovi/login.html");
+		}
+		
+		
 	}
 
 }
--- src/service/LoginService.java
+++ src/service/LoginService.java
@@ -1,5 +1,18 @@
 package service;
 
+import model.User;
+
+import javax.transaction.Transactional;
+
+import dao.LoginDAO;
+
 public class LoginService {
 
+	LoginDAO dao = new LoginDAO();
+
+	@Transactional
+	public User vratiUseraLogin(String userName, String password) {
+		
+		return dao.vratiUseraLogin(userName, password);
+	}
 }
